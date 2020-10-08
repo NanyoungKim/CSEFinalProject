5dc7fdaeb9e7fee191906d4e25ead0bcff97d543
Nanyoung Kim <nanyoung18@gmail.com>
2020-10-08 15:35:39 +0900

    openCV 카메라 출력

--- app/src/main/java/com/example/csefinalproject/CameraActivity.java
+++ app/src/main/java/com/example/csefinalproject/CameraActivity.java
@@ -128,6 +128,7 @@ public class CameraActivity extends AppCompatActivity
 //
 //        ConvertRGBtoGray(matInput.getNativeObjAddr(), matResult.getNativeObjAddr());
 
+        //
         return matInput;
     }
 
