4866d29ea875a568b5b1bd32af3b931f44a80e83
Nanyoung Kim <nanyoung18@gmail.com>
2020-10-08 14:20:15 +0900

    openCV 올리기 완료

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -15,9 +15,9 @@ using namespace cv;
 
 extern "C"
 JNIEXPORT void JNICALL
-Java_com_example_csefinalproject_MainActivity_ConvertRGBtoGray(JNIEnv *env, jobject thiz,
-                                                               jlong mat_addr_input,
-                                                               jlong mat_addr_result) {
+Java_com_example_csefinalproject_CameraActivity_ConvertRGBtoGray(JNIEnv *env, jobject thiz,
+                                                                 jlong mat_addr_input,
+                                                                 jlong mat_addr_result) {
     // TODO: implement ConvertRGBtoGray()
 
     Mat &matInput = *(Mat *)mat_addr_input;
